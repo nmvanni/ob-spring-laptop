04815e3de3dc52e47d74be58ddb7a299e02c492e
nmvanni <vannimartineznicolas@gmail.com>
2023-06-09 13:45:54 -0300

    Creación del proyecto con Spring Initialzr

---
+++ src/test/java/com/example/obrestdatajpalaptop/ObRestDatajpaLaptopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obrestdatajpalaptop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObRestDatajpaLaptopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
